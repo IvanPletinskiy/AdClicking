4ef5941e4496dc750bd977c70d2c0de756bd6d1d
Handen Laptop <handeninc@gmail.com>
2019-05-10 00:30:25 +0300

    Initial commit

---
+++ venv/Main.py
@@ -0,0 +1,17 @@
+import pyautogui
+from PIL import ImageGrab
+from time import sleep
+
+
+def watchAd():
+    pass
+
+
+def initialize():
+    pass
+
+
+if __name__ == '__main__':
+    initialize()
+    while True:
+        watchAd()
