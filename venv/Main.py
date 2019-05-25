9f108294cb78c14010a80ddae9f0904d51cdfc7a
DebDoDab <debbius3@gmail.com>
2019-05-25 07:41:15 -0700

    many fixes

--- venv/Main.py
+++ venv/Main.py
@@ -1,11 +1,14 @@
 # coding=utf-8
+from sys import stdout
+
 import numpy
 import pyautogui
 import win32gui
-from PIL import ImageGrab
+from PIL import ImageGrab, ImageDraw
 from time import sleep
 import random
 from pynput.mouse import Button, Controller
+
 
 class Rectangle:
     def __init__(self, x, y, width, height):
@@ -56,17 +59,11 @@ class Device:
         self.y = min(30, dimensions[1])
         self.w = 490
         self.h = 970
-        # win32gui.MoveWindow(self.hwnd, self.x, self.y, self.x + self.w, self.y + self.h, True)
-        # dimensions = win32gui.GetWindowRect(self.hwnd)
-        # print("Dimensions =", dimensions)
-        # self.x = dimensions[0]
-        # self.y = dimensions[1]
-        # self.w = dimensions[2] - self.x
-        # self.h = dimensions[3] - self.y
-        self.click("AppIcon")
+        # self.click("AppIcon")
 
     # click on the button in random position
     def click(self, name):
+        win32gui.SetForegroundWindow(self.hwnd)
         rect = Rectangles.getCoordinates(name)
         x = random.uniform(rect.x, rect.x + rect.width)
         y = random.uniform(rect.y, rect.y + rect.height)
@@ -74,8 +71,9 @@ class Device:
         print("Click: " + name + " " + str(int(x)) + " " + str(int(y)))
 
     def clickCoordinates(self, _x, _y, _w, _h):
-        x = random.uniform(_x, _x + _w)
-        y = random.uniform(_y, _y + _h)
+        win32gui.SetForegroundWindow(self.hwnd)
+        x = int(random.uniform(_x, _x + _w))
+        y = int(random.uniform(_y, _y + _h))
         pyautogui.click(self.x + x, self.y + y)
         print("Click: AdInstall " + str(int(x)) + " " + str(int(y)))
 
@@ -94,7 +92,7 @@ class Device:
         win32gui.SetForegroundWindow(self.hwnd)
         self.image = ImageGrab.grab((self.x, self.y, self.x + self.w, self.y + self.h))
         # self.image.show()
-        self.pixels = numpy.array(self.image.getdata()).reshape(self.image.size[0], self.image.size[1], 3)
+        self.pixels = numpy.array(self.image)
 
     # check if the video is going on
     def checkAdShowing(self):
@@ -125,14 +123,24 @@ class Device:
         img = self.pixels
         centerY = (self.y + self.h) / 2
         centerX = (self.x + self.w) / 2
+        # TODO
+
+    def showDebugPic(self, x, y, w, h):
+        self.getScreen()
+        img = self.image
+        draw = ImageDraw.Draw(img)
+        draw.rectangle([x, y, x + w, y + h], outline=255)
+        del draw
+        img.show()
 
     def findAndClickInstallButton(self):
         self.getScreen()
         img = self.pixels
-        #Находим зелёные кнопки
+        print("Lengthes: ", len(img), len(img[0]))
+        # Находим зелёные кнопки
         for i in range(len(img)):
             for j in range(len(img[i])):
-                #Чек на пиксель зелёный
+                # Чек на пиксель зелёный
                 if self.checkPixelGreen(img, i, j):
                     widthB = 1
                     heightB = 1
@@ -144,15 +152,17 @@ class Device:
                         if not self.checkPixelGreen(img, i, j1):
                             break
                         widthB += 1
-                    if(widthB + heightB < 70):
+                    if widthB + heightB < 200 or not self.greenPixelsAmountInside(img, j, i, widthB, heightB) > widthB * heightB * 0.7:
                         continue
-                    self.clickCoordinates(i, j, widthB, heightB)
+                    print(f"Found green button on {j, i, widthB, heightB}")
+                    self.clickCoordinates(j, i, widthB, heightB)
+                    self.showDebugPic(j, i, widthB, heightB)
                     return
 
-        #Находим красные кнопки
+        # Находим красные кнопки
         for i in range(len(img)):
             for j in range(len(img[i])):
-                #Чек на пиксель красный
+                # Чек на пиксель красный
                 if self.checkPixelGreen(img, i, j):
                     widthB = 1
                     heightB = 1
@@ -164,20 +174,75 @@ class Device:
                         if not self.checkPixelGreen(img, i, j1):
                             break
                         widthB += 1
-                    if (widthB + heightB < 70):
+                    if widthB + heightB < 200 or not self.redPixelsAmountInside(img, j, i, widthB, heightB) > widthB * heightB * 0.7:
                         continue
-                    self.clickCoordinates(i, j, widthB, heightB)
+                    print(f"Found red button on {j, i, widthB, heightB}")
+                    self.clickCoordinates(j, i, widthB, heightB)
+                    self.showDebugPic(j, i, widthB, heightB)
                     return
 
-        self.clickCoordinates((self.x  + self.w) / 2, (self.y  + self.h) / 2, 50, 50)
+        # Находим голубые кнопки
+        for i in range(len(img)):
+            for j in range(len(img[i])):
+                # Чек на пиксель голубой
+                if self.checkPixelBlue(img, i, j):
+                    widthB = 1
+                    heightB = 1
+                    for i1 in range(i + 1, len(img)):
+                        if not self.checkPixelBlue(img, i1, j):
+                            break
+                        heightB += 1
+                    for j1 in range(j + 1, len(img[i])):
+                        if not self.checkPixelBlue(img, i, j1):
+                            break
+                        widthB += 1
+                    if widthB + heightB < 200 or not self.bluePixelsAmountInside(img, j, i, widthB, heightB) > widthB * heightB * 0.7:
+                        continue
+                    print(f"Found blue button on {j, i, widthB, heightB}")
+                    self.clickCoordinates(j, i, widthB, heightB)
+                    self.showDebugPic(j, i, widthB, heightB)
+                    return
+
+
+        print(f"Didn't find anything, click on the center")
+        self.clickCoordinates(self.w // 2, self.h // 2, 50, 50)
+
+    def greenPixelsAmountInside(self, img, x, y, width, height):
+        cnt = 0
+        for i in range(y, y + height):
+            for j in range(x, x + width):
+                if self.checkPixelGreen(img, i, j):
+                    cnt += 1
+        return cnt
+
+    def redPixelsAmountInside(self, img, x, y, width, height):
+        cnt = 0
+        for i in range(y, y + height):
+            for j in range(x, x + width):
+                if self.checkPixelRed(img, i, j):
+                    cnt += 1
+        return cnt
+
+    def bluePixelsAmountInside(self, img, x, y, width, height):
+        cnt = 0
+        for i in range(y, y + height):
+            for j in range(x, x + width):
+                if self.checkPixelBlue(img, i, j):
+                    cnt += 1
+        return cnt
 
     def checkPixelGreen(self, img, y, x):
-        if (img[y, x, 0] < 200 and img[y, x, 1] > 240 and img[y, x, 2] < 200):
+        if int(img[y, x, 1]) - int(img[y, x, 0]) >= 50 and int(img[y, x, 1]) >= 150 and int(img[y, x, 1]) - int(img[y, x, 2]) >= 50:
             return True
         return False
 
     def checkPixelRed(self, img, y, x):
-        if (img[y, x, 0] > 240 and img[y, x, 1] < 200 and img[y, x, 2] < 200):
+        if int(img[y, x, 0]) >= 150 and int(img[y, x, 0]) - int(img[y, x, 1]) >= 50 and int(img[y, x, 0]) - int(img[y, x, 2]) >= 50:
+            return True
+        return False
+
+    def checkPixelBlue(self, img, y, x):
+        if int(img[y, x, 2]) - int(img[y, x, 0]) >= 50 and int(img[y, x, 2]) - int(img[y, x, 1]) >= 50 and int(img[y, x, 2]) >= 150:
             return True
         return False
 
@@ -194,21 +259,28 @@ class Device:
         self.randomSleep(0)
 
         # wait for the ad to start
-        self.isAdShowing = False
-        while not self.isAdShowing:
+        isAdShowing = False
+        while not isAdShowing:
             self.click("AdButton")
             self.randomSleep(3)
-            self.isAdShowing = self.checkAdShowing()
+            isAdShowing = self.checkAdShowing()
 
-        # wait for the ad to end
-        while self.isAdShowing:
-            self.randomSleep(5)
-            self.isAdShowing = self.checkAdShowing()
+        # Doesn't work with gifs
+        
+        # # wait for the ad to end
+        # while isAdShowing:
+        #     self.randomSleep(5)
+        #     isAdShowing = self.checkAdShowing()
+        
+        self.randomSleep(40)
+
 
         # check if we downloaded this previously
         if self.checkAdPreviouslyClicked():
-            pass
+            return
+
         self.findAndClickInstallButton()
+        self.randomSleep(3)
         # install
         self.click("InstallGooglePlay")
         self.randomSleep(5)
@@ -219,11 +291,11 @@ class Device:
 
         # download
         self.click("DownloadButton")
-        self.isDownloaded = False
+        isDownloaded = False
 
         # wait for the app to download and install
-        while not self.isDownloaded:
-            self.isDownloaded = self.checkIsDownloaded()
+        while not isDownloaded:
+            isDownloaded = self.checkIsDownloaded()
             self.randomSleep(10)
 
         # do some fake actions
@@ -237,7 +309,7 @@ class Device:
         self.fakeActivity()
 
 
-# function for debbuging and finding rectangles
+# function for debugging and finding rectangles
 def mouseTrack():
 
     mouse = Controller()
@@ -249,7 +321,7 @@ def mouseTrack():
 
 
 if __name__ == '__main__':
-    #mouseTrack()
+    # mouseTrack()
     firstDevice = Device(0)
     while True:
         firstDevice.watchAd()
